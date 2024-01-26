4603c476a92b37c0b534cb716e88dc68a12f2928
davidnongang <nongangdavid@gmail.com>
2024-01-26 14:15:59 -0500

    commit #7

---
+++ tkinter/tkinterStylingLesson.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+from tkinter import ttk
+
+# Window
+window = tk.Tk()
+window.title('Styling')
+window.geometry('400x300')
+
+
+# Widgets
+label = ttk.Label()
+
+
+# Run
+window.mainloop()
